74929f0cd06b38c6e4c22a49df1cfb2276b3b753
Jeffrey Bergier <jeffburg@jeffburg.com>
2020-04-26 17:35:15 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,6 +1,16 @@
+// swift-tools-version:4.2
 import PackageDescription
 
 let package = Package(
-    name: "SimpleImageViewer",
-    dependencies : []
+    name: "Cheers",
+    // platforms: [.iOS("8.0"), .macOS("10.13"), .tvOS("9.2")],
+    products: [
+        .library(name: "SimpleImageViewer", targets: ["SimpleImageViewer"])
+    ],
+    targets: [
+        .target(
+            name: "SimpleImageViewer",
+            path: "ImageViewer"
+        )
+    ]
 )
